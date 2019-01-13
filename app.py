c04702e32c5028c3b6a11531e399ceb093e0b865
Brian Smith <Brian.Smith@microsoft.com>
2019-01-13 13:22:02 -0800

    Added Golf to Sports array
    Added missing item from sports array - Golf

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ app = Flask(__name__)
 
 path = Path("path")
 classes =  ['aussie','baseball','basketball','camogie','cricket','football','gaelic','golf','hockey','hurling','icehockey','lacrosse','other','quidditch','rugby','soccer','tennis']
-sports = ['Australian Rules Football', 'Baseball', 'Basketball', 'Camogie', 'Cricket', 'American Football', 'Gaelic Football', 'Field Hockey', 'hurling', 'Ice Hockey', 'Lacrosse', 'not a sport','Quidditch', 'Rugby', 'Soccer', 'Tennis']
+sports = ['Australian Rules Football', 'Baseball', 'Basketball', 'Camogie', 'Cricket', 'American Football', 'Gaelic Football', 'Golf' 'Field Hockey', 'hurling', 'Ice Hockey', 'Lacrosse', 'not a sport','Quidditch', 'Rugby', 'Soccer', 'Tennis']
 data2 = ImageDataBunch.single_from_classes(path, classes, tfms=get_transforms(), size=448).normalize(imagenet_stats)
 learn = create_cnn(data2, models.resnet34)
 learn.load('LargeModel10Own', strict=False, with_opt=False)
